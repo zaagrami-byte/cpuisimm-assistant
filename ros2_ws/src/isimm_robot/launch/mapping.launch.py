a6ecdadf9781057f8d0cc3944be5b30e6d14c75c
zaagrami-byte <zaagrami@gmail.com>
2026-09-26 12:40:31 +0100

    configure map and activate slamtools in lanch files

--- ros2_ws/src/isimm_robot/launch/mapping.launch.py
+++ ros2_ws/src/isimm_robot/launch/mapping.launch.py
@@ -12,10 +12,33 @@ def generate_launch_description():
     slam_params = os.path.join(pkg, 'config', 'slam_params.yaml')
 
     return LaunchDescription([
+        # Robot base: motors, sensors, RPLIDAR, RF2O, TF...
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(pkg, 'launch', 'robot.launch.py'))),
+                os.path.join(pkg, 'launch', 'robot.launch.py')
+            )
+        ),
 
-        Node(package='slam_toolbox', executable='async_slam_toolbox_node',
-             name='slam_toolbox', parameters=[slam_params], output='screen'),
+        # SLAM Toolbox
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            parameters=[slam_params],
+            output='screen',
+        ),
+
+        # Automatically configure + activate SLAM Toolbox
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_slam',
+            output='screen',
+            parameters=[
+                {
+                    'autostart': True,
+                    'node_names': ['slam_toolbox'],
+                }
+            ],
+        ),
     ])
